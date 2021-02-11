99a04b9e336ae30689aa73544744c6e64a8eebc3
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-11 21:41:07 +0530

    Made required files. Installed required modules

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion: 6 
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+ res.send("HEllo!");
+});
+
+
+app.listen(3000, function(){
+	console.log("Server running on port 3000");
+});
