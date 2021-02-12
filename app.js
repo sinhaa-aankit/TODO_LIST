34f6f379fb90acddead656343ae6c94f77c1eebe
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-12 08:37:58 +0530

    Checked if current date is Weekend or not

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
- res.send("HEllo!");
+	var today = new Date();
+
+	if(today.getDay() === 6 || today.getDay() === 0){
+		res.send("Enjoy! It's the weekend");
+	}else{
+		res.send("Boo! I have to work");
+	}
 });
 
 
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
-
-//
