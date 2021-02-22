7da068b7a3e8dfbadaf9418669564938a9960e4c
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-22 09:12:24 +0530

    changed date variable to show full date and added a input field to add TODO text into app.js

--- app.js
+++ app.js
@@ -15,25 +15,16 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
 
 	var today = new Date();
-	var currentDay = today.getDay();
-	var day = "";
+	
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long"
+	};
 
-	if(currentDay === 0){
-		day = "Sunday";
-	}else if(currentDay === 1){
-		day = "Monday";
-	}else if(currentDay === 2){
-		day = "Tuesday";
-	}
-	else if(currentDay === 3){
-		day = "Wednesday";
-	}else if(currentDay === 4){
-		day = "Thursday";
-	}else if(currentDay === 5){
-		day = "Friday";
-	}else{
-		day = "Saturday";
-	}
+	var day = today.toLocaleDateString("en-US", options);
+
+
 	res.render("list", {anyDay: day});
 });
 
