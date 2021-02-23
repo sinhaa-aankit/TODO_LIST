8e073c28e08823f38b92be6aeaef297b1bc23d73
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-02-23 09:07:57 +0530

    Made a public folder and made it as static resource from app.js. Change the design by adding css & it looks cool now.

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var todoItems = [];
+let todoItems = [];
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
-
 app.get("/", function(req, res){
 
-	var today = new Date();
+	let today = new Date();
 	
-	var options = {
+	let options = {
 		weekday: "long",
 		day: "numeric",
 		month: "long"
 	};
 
-	var day = today.toLocaleDateString("en-US", options);
+	let day = today.toLocaleDateString("en-US", options);
 
 
 	res.render("list", {anyDay: day, newTodoItems: todoItems});
 });
 
 app.post("/", function(req,res){
-	var item = req.body.newItem;
+	let item = req.body.newItem;
 	todoItems.push(item);
 
 	res.redirect("/");
